07876be4401219040dea2540a67d5b264ce9f22d
rmansoor <rmansoor@pentaho.com>
2020-07-30 23:34:31 -0400

    [TAP-908] - Using PVFS connection to be use when executing in shim layer

--- common-fragment-V1/src/main/java/org/pentaho/hadoop/shim/common/format/S3NCredentialUtils.java
+++ common-fragment-V1/src/main/java/org/pentaho/hadoop/shim/common/format/S3NCredentialUtils.java
@@ -22,9 +22,11 @@
 package org.pentaho.hadoop.shim.common.format;
 
 import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.pentaho.amazon.s3.PentahoDefaultAWSCredentialsProviderChain;
+import org.pentaho.amazon.s3.PentahoVFSAwareCredentials;
+import org.pentaho.amazon.s3.PentahoVFSAwareCredentialsProvider;
 
 import java.net.URI;
 
@@ -37,32 +39,41 @@ public class S3NCredentialUtils {
   private static boolean s3nIsSupported = true;
 
   public static void applyS3CredentialsToHadoopConfigurationIfNecessary( String filename, Configuration conf ) {
-    Path outputFile = new Path( scrubFilePathIfNecessary( filename ) );
+    AWSCredentials credentials; Path outputFile = new Path( scrubFilePathIfNecessary( filename ) );
     URI uri = outputFile.toUri();
-    String scheme = uri != null ? uri.getScheme() : null;
-    if ( scheme != null && scheme.equals( S3NSCHEME ) ) {
-      AWSCredentials credentials = DefaultAWSCredentialsProviderChain.getInstance().getCredentials();
-      conf.set( "fs.s3n.awsAccessKeyId", credentials.getAWSAccessKeyId() );
-      conf.set( "fs.s3n.awsSecretAccessKey", credentials.getAWSSecretKey() );
-      conf.set( "fs.s3.buffer.dir", System.getProperty( "java.io.tmpdir" ) );
+    String scheme = uri != null ? uri.getScheme() : null; if ( scheme != null && scheme.equals( S3NSCHEME ) ) {
+      credentials = PentahoDefaultAWSCredentialsProviderChain.getInstance().getCredentials();
+      if ( credentials != null ) {
+        conf.set( "fs.s3n.awsAccessKeyId", credentials.getAWSAccessKeyId() );
+        conf.set( "fs.s3n.awsSecretAccessKey", credentials.getAWSSecretKey() );
+        conf.set( "fs.s3.buffer.dir", System.getProperty( "java.io.tmpdir" ) );
+      }
+    } else if ( scheme != null && scheme.equals( S3ASCHEME ) ) {
+      PentahoVFSAwareCredentialsProvider credentialsProvider = new PentahoVFSAwareCredentialsProvider();
+      credentials = credentialsProvider.getCredentials();
+      if ( credentials != null && credentials instanceof PentahoVFSAwareCredentials ) {
+        PentahoVFSAwareCredentials vfsAwareCredentials = (PentahoVFSAwareCredentials) credentials;
+        conf.set( "fs.s3a.path.style.access", Boolean.toString( vfsAwareCredentials.getPathStyleAccess() ) );
+        conf.set( "fs.s3a.secret.key", vfsAwareCredentials.getAWSSecretKey() );
+        conf.set( "fs.s3a.access.key", vfsAwareCredentials.getAWSAccessKeyId() );
+        conf.set( "fs.s3a.endpoint", vfsAwareCredentials.getEndpointUrl() );
+      }
     }
   }
 
   public static String scrubFilePathIfNecessary( String filename ) {
     if ( filename != null ) {
-      filename = filename.replace( S3NROOTBUCKET, "" );
-      if ( !s3nIsSupported ) {
+      filename = filename.replace( S3NROOTBUCKET, "" ); if ( !s3nIsSupported ) {
         filename = filename.replace( S3NSCHEME, S3ASCHEME );
       }
-    }
-    return filename;
-  }
-
-  public static void setS3nIsSupported( boolean supported ) {
-    s3nIsSupported = supported;
+    } return filename;
   }
 
   public static boolean isS3nIsSupported() {
     return s3nIsSupported;
+  }
+
+  public static void setS3nIsSupported( boolean supported ) {
+    s3nIsSupported = supported;
   }
 }
